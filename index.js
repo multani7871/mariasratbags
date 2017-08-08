9a8a5935519f83fb103e7c9b2f54cccc5594cbda
Harvey Multani <harvey.multani@gmail.com>
2017-08-08 12:54:20 -0700

    add set

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ app.get('/', function(req, res) {
 });
 
 var port = process.env.PORT || 3000;
+app.set('port', port);
 
 app.listen(port, function() {
   console.log('Example app listening on port 3000!');
